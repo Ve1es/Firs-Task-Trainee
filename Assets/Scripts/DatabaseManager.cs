155f952f60a28dd16b8f1c3d362a1998813d2783
Ve1es <15201520n@gmail.com>
2024-04-08 19:01:05 +0300

    Scripts for work with firebase
    Add methot for set and get data in firebase

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -1,19 +1,51 @@
 using Firebase.Database;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string _userID;
+    private DatabaseReference _databaseReference;
+
+    private string _userName;
+    private string _userPassword;
+    private string _userEmail;
+    private string _userMaxScore;
     void Start()
     {
-        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        _userID = SystemInfo.deviceUniqueIdentifier;
+        _databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateUser(string name, string email, string password)
     {
-        
+        User NewUser = new User(name, email, password);
+        string Json = JsonUtility.ToJson(NewUser);
+
+        _databaseReference.Child("users").Child(_userID).SetRawJsonValueAsync(Json);
+    }
+
+    public IEnumerator GetData(Action<string> onCallback, string fieldName)
+    {
+        var UserData = _databaseReference.Child("users").Child(_userID).Child(fieldName).GetValueAsync();
+        yield return new WaitUntil(predicate: () => UserData.IsCompleted);
+
+        if(UserData!=null)
+        {
+            DataSnapshot Snapshot = UserData.Result;
+
+            onCallback.Invoke((string)Snapshot.Value);
+        }
+    }
+
+    public string GetInfo(string fieldName) {
+        string nameData="";
+        StartCoroutine(GetData((string name) =>
+        {
+            nameData = name;
+        }, fieldName));
+        return nameData;
     }
 }
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,14 @@
+
+public class User 
+{
+    public string Name;
+    public string Email;
+    public string Password;
+
+    public User(string name, string email, string password)
+    {
+        this.Name = name;
+        this.Email = email;
+        this.Password = password;
+    }
+}
